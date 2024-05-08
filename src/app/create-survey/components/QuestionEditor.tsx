b0cab10db83b3bd0197412e9579b30fb06817065
emilio <emilio5139@gmail.com>
2024-05-08 19:10:43 -0400

    first commit

--- src/app/create-survey/components/QuestionEditor.tsx
+++ src/app/create-survey/components/QuestionEditor.tsx
@@ -28,11 +28,16 @@ const QuestionEditor: React.FC<QuestionEditorProps> = ({
         setSurvey(updatedSurvey);
     };
 
-    const handleQuestionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleQuestionChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
         setQuestion(event.target.value);
     };
 
-    const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>, optionIndex: number) => {
+    const handleOptionChange = (
+        event: React.ChangeEvent<HTMLInputElement>,
+        optionIndex: number
+    ) => {
         const updatedOptions = [...options];
         updatedOptions[optionIndex] = event.target.value;
         setOptions(updatedOptions);
@@ -42,7 +47,10 @@ const QuestionEditor: React.FC<QuestionEditorProps> = ({
         handleUpdateSurvey();
     };
 
-    const handleInputKeyPress = (event: React.KeyboardEvent<HTMLInputElement>, optionIndex: number) => {
+    const handleInputKeyPress = (
+        event: React.KeyboardEvent<HTMLInputElement>,
+        optionIndex: number
+    ) => {
         if (event.key === "Enter") {
             event.preventDefault(); // Prevent form submission
             handleUpdateSurvey();
@@ -59,14 +67,24 @@ const QuestionEditor: React.FC<QuestionEditorProps> = ({
         } else if (event.key === "ArrowUp" && optionIndex > 0) {
             event.preventDefault();
             optionRefs.current[optionIndex - 1]?.focus(); // Focus the previous input
-        } else if (event.key === "ArrowDown" && optionIndex + 1 < options.length) {
+        } else if (
+            event.key === "ArrowDown" &&
+            optionIndex + 1 < options.length
+        ) {
             event.preventDefault();
             optionRefs.current[optionIndex + 1]?.focus(); // Focus the next input
         }
     };
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", width: "100%", marginBottom: '2rem', }}>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                marginBottom: "2rem",
+            }}
+        >
             <div className="question" style={{ display: "flex" }}>
                 {index + 1}.&nbsp;
                 <input
@@ -78,19 +96,46 @@ const QuestionEditor: React.FC<QuestionEditorProps> = ({
                     onBlur={handleInputBlur}
                 />
             </div>
-            <div style={{ display: "flex", flexDirection: "column", gap: '1rem', marginTop: '1.2rem' }}>
-                {options.map((option, optionIndex) => (
-                    <div key={optionIndex} style={{ display: "flex", marginLeft: "1rem" }}>
-                        <div style={{ height: "1rem", width: "1rem", borderRadius: "100%", border: "1px solid black", opacity: option === "" ? 0.4 : 1 }} />
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1rem",
+                    marginTop: "1.2rem",
+                }}
+            >
+                {options.map((option: string, optionIndex: number) => (
+                    <div
+                        key={optionIndex}
+                        style={{ display: "flex", marginLeft: "1rem" }}
+                    >
+                        <div
+                            style={{
+                                height: "1rem",
+                                width: "1rem",
+                                borderRadius: "100%",
+                                border: "1px solid black",
+                                opacity: option === "" ? 0.4 : 1,
+                            }}
+                        />
                         <input
-                            ref={(el) => optionRefs.current[optionIndex] = el}
-                            style={{ marginLeft: "0.6rem", opacity: option === "" ? 0.4 : 1 }}
+                            ref={(el) => {
+                                optionRefs.current[optionIndex] = el; // Properly update the refs array without returning anything
+                            }}
+                            style={{
+                                marginLeft: "0.6rem",
+                                opacity: option === "" ? 0.4 : 1,
+                            }}
                             type="text"
                             placeholder="Type option label"
                             value={option}
-                            onChange={(event) => handleOptionChange(event, optionIndex)}
+                            onChange={(event) =>
+                                handleOptionChange(event, optionIndex)
+                            }
                             onBlur={handleInputBlur}
-                            onKeyDown={(event) => handleInputKeyPress(event, optionIndex)}
+                            onKeyDown={(event) =>
+                                handleInputKeyPress(event, optionIndex)
+                            }
                         />
                     </div>
                 ))}
